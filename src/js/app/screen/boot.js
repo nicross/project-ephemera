50d7482a4480de96d68a6ab0ec67bfc15c913921
nicross <nicross@users.noreply.github.com>
2023-12-15 16:41:11 -0600

    Add boot screen

---
+++ src/js/app/screen/boot.js
@@ -0,0 +1,53 @@
+app.screen.splash = app.screenManager.invent({
+  // Attributes
+  id: 'boot',
+  parentSelector: '.a-app--boot',
+  rootSelector: '.a-boot',
+  transitions: {
+    done: function () {
+      this.change('game')
+    },
+  },
+  // Hooks
+  onEnter: async function () {
+    this.statusElement = this.rootElement.querySelector('.a-boot--status')
+
+    await this.doDisclaimer()
+    await this.doLogo()
+
+    app.screenManager.dispatch('done')
+  },
+  // Methods
+  doDisclaimer: async function () {
+    await engine.fn.promise(1 * 1000)
+
+    const text = 'For demonstration purposes only.'
+
+    const disclaimer = document.createElement('div')
+    disclaimer.classList.add('a-boot--disclaimer')
+    disclaimer.innerHTML = text
+
+    this.rootElement.appendChild(disclaimer)
+    this.statusElement.innerHTML = text
+  },
+  doLogo: async function () {
+    await engine.fn.promise(1 * 1000)
+
+    const text = 'Ephemera'
+
+    const logo = document.createElement('div')
+    logo.classList.add('a-boot--logo')
+    logo.setAttribute('aria-label', text)
+
+    logo.innerHTML = text.split('').map(
+      (letter) => `<span class="a-boot--logoLetter"><span class="a-boot--logoLetterInner">${letter}</span></span>`
+    ).join('')
+
+    this.rootElement.appendChild(logo)
+
+    await engine.fn.promise(1 * 1000)
+    this.statusElement.innerHTML = text
+
+    await engine.fn.promise(4 * 1000)
+  },
+})
--- src/js/app/screen/splash.js
+++ src/js/app/screen/splash.js
@@ -5,13 +5,8 @@ app.screen.splash = app.screenManager.invent({
   rootSelector: '.a-splash',
   transitions: {
     interact: function () {
-      this.change('game')
+      this.change('boot')
     },
-  },
-  // State
-  state: {
-    idle: false,
-    idleTimeout: 0,
   },
   // Hooks
   onReady: function () {
@@ -23,40 +18,11 @@ app.screen.splash = app.screenManager.invent({
 
     root.querySelector('.a-splash--version').innerHTML = `v${app.version()}`
   },
-  onEnter: function () {
-    this.setIdle(false)
-  },
   onFrame: function () {
     const ui = app.controls.ui()
 
     if (ui.confirm || ui.enter || ui.space || ui.start || ui.focus === 0) {
       app.screenManager.dispatch('interact')
-    }
-
-    const isMoved = engine.input.mouse.getMoveX()
-      || engine.input.mouse.getMoveY()
-      || engine.input.gamepad.getAxis(0)
-      || engine.input.gamepad.getAxis(1)
-      || engine.input.gamepad.getAxis(2)
-      || engine.input.gamepad.getAxis(3)
-
-    if (!this.state.idle && engine.time() >= this.state.idleTimeout) {
-      this.setIdle(true)
-    } else if (isMoved) {
-      this.setIdle(false)
-    }
-  },
-  // Methods
-  setIdle: function (state) {
-    const root = this.rootElement
-
-    this.state.idle = state
-
-    if (this.state.idle) {
-      root.classList.add('a-splash-idle')
-    } else {
-      this.state.idleTimeout = engine.time() + 8
-      root.classList.remove('a-splash-idle')
     }
   },
 })
