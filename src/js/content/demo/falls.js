b32da3cde97700db64fb4f652141461ecf7daad6
nicross <nicross@users.noreply.github.com>
2023-12-20 20:17:57 -0600

    Begin first level

---
+++ src/js/content/demo/falls.js
@@ -0,0 +1,6 @@
+content.demo.falls = content.demo.register({
+  id: 'falls',
+  enabled: true,
+  description: 'Patent pending.',
+  name: 'Harmony Falls',
+})
--- src/js/content/demo/test1.js
+++
@@ -1,5 +0,0 @@
-content.demo.test1 = content.demo.register({
-  id: 'test1',
-  description: 'Inspired by Tempest.',
-  name: 'Harmony Falls',
-})
