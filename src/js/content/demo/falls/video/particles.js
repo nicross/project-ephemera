19ddb0d24ebe941560f5c386612fe7e2ed2493a8
nicross <nicross@users.noreply.github.com>
2023-12-23 13:07:39 -0600

    Falls: Add particles when moving

--- src/js/content/demo/falls/video/particles.js
+++ src/js/content/demo/falls/video/particles.js
@@ -57,81 +57,107 @@ content.demo.falls.video.particles = (() => {
   }
 })()
 
-engine.ready(() => {
+// Enemy hits
+content.demo.falls.enemies.on('hit', ({
+  enemy,
+}) => {
+  const context = content.demo.falls.video.context()
 
-  content.demo.falls.enemies.on('hit', ({
-    enemy,
-  }) => {
-    const context = content.demo.falls.video.context()
+  if (!context) {
+    return
+  }
 
-    if (!context) {
-      return
-    }
+  const count = engine.fn.randomFloat(20, 30)
+  const velocity = context.canvas.height * 100/600
 
-    const count = engine.fn.randomFloat(20, 30)
-    const velocity = context.canvas.height * 100/600
+  for (let i = 0; i <= count; i += 1) {
+    content.demo.falls.video.particles.spawn({
+      life: 1,
+      offset: {x: engine.fn.randomFloat(-2 * context.canvas.height/600, 2 * context.canvas.height/600), y: 0},
+      radius: engine.fn.randomFloat(0, 1) ** 2,
+      rate: engine.fn.randomFloat(2, 4),
+      velocity: {x: velocity * engine.fn.randomFloat(-1, 1), y: velocity * engine.fn.randomFloat(0, 1)},
+      x: enemy.x,
+      y: enemy.y,
+    })
+  }
+})
 
-    for (let i = 0; i <= count; i += 1) {
-      content.demo.falls.video.particles.spawn({
-        life: 1,
-        offset: {x: engine.fn.randomFloat(-2 * context.canvas.height/600, 2 * context.canvas.height/600), y: 0},
-        radius: engine.fn.randomFloat(0, 1) ** 2,
-        rate: engine.fn.randomFloat(2, 4),
-        velocity: {x: velocity * engine.fn.randomFloat(-1, 1), y: velocity * engine.fn.randomFloat(0, 1)},
-        x: enemy.x,
-        y: enemy.y,
-      })
-    }
-  })
+// Enemy kills
+content.demo.falls.enemies.on('kill', ({
+  enemy,
+}) => {
+  const context = content.demo.falls.video.context()
 
-  content.demo.falls.enemies.on('kill', ({
-    enemy,
-  }) => {
-    const context = content.demo.falls.video.context()
+  if (!context) {
+    return
+  }
 
-    if (!context) {
-      return
-    }
+  const count = engine.fn.randomFloat(50, 100)
+  const velocity = context.canvas.height * 100/600
 
-    const count = engine.fn.randomFloat(50, 100)
-    const velocity = context.canvas.height * 100/600
+  for (let i = 0; i <= count; i += 1) {
+    content.demo.falls.video.particles.spawn({
+      life: 1,
+      offset: {x: engine.fn.randomFloat(-context.canvas.height/17/2, context.canvas.height/17/2), y: 0},
+      radius: engine.fn.randomFloat(0, 2),
+      rate: engine.fn.randomFloat(1, 2),
+      velocity: {x: velocity * engine.fn.randomFloat(-1, 1), y: velocity * engine.fn.randomFloat(0, 1)},
+      x: enemy.x,
+      y: 1,
+    })
+  }
+})
 
-    for (let i = 0; i <= count; i += 1) {
-      content.demo.falls.video.particles.spawn({
-        life: 1,
-        offset: {x: engine.fn.randomFloat(-context.canvas.height/17/2, context.canvas.height/17/2), y: 0},
-        radius: engine.fn.randomFloat(0, 2),
-        rate: engine.fn.randomFloat(1, 2),
-        velocity: {x: velocity * engine.fn.randomFloat(-1, 1), y: velocity * engine.fn.randomFloat(0, 1)},
-        x: enemy.x,
-        y: 1,
-      })
-    }
-  })
+// Player kills
+content.demo.falls.player.on('kill', () => {
+  const context = content.demo.falls.video.context()
 
-  content.demo.falls.player.on('kill', () => {
-    const context = content.demo.falls.video.context()
+  if (!context) {
+    return
+  }
 
-    if (!context) {
-      return
-    }
+  const count = engine.fn.randomFloat(50, 100),
+    x = content.demo.falls.player.x()
 
-    const count = engine.fn.randomFloat(50, 100),
-      x = content.demo.falls.player.x()
+  const velocity = context.canvas.height * 100/600
 
-    const velocity = context.canvas.height * 100/600
+  for (let i = 0; i <= count; i += 1) {
+    content.demo.falls.video.particles.spawn({
+      life: 1,
+      offset: {x: engine.fn.randomFloat(-context.canvas.height/17/2, context.canvas.height/17/2), y: 0},
+      radius: engine.fn.randomFloat(0, 2),
+      rate: engine.fn.randomFloat(1, 2),
+      velocity: {x: velocity * engine.fn.randomFloat(-1, 1), y: velocity * engine.fn.randomFloat(-1, 0)},
+      x,
+      y: 0,
+    })
+  }
+})
 
-    for (let i = 0; i <= count; i += 1) {
-      content.demo.falls.video.particles.spawn({
-        life: 1,
-        offset: {x: engine.fn.randomFloat(-context.canvas.height/17/2, context.canvas.height/17/2), y: 0},
-        radius: engine.fn.randomFloat(0, 2),
-        rate: engine.fn.randomFloat(1, 2),
-        velocity: {x: velocity * engine.fn.randomFloat(-1, 1), y: velocity * engine.fn.randomFloat(-1, 0)},
-        x,
-        y: 0,
-      })
-    }
-  })
+// Player moves
+content.demo.falls.player.on('move', () => {
+  const context = content.demo.falls.video.context()
 
+  if (!context) {
+    return
+  }
+
+  const count = engine.fn.randomFloat(20, 30),
+    moveDirection = content.demo.falls.input.moveDirection(),
+    x = content.demo.falls.player.x()
+
+  const velocity = context.canvas.height * 100/600
+
+  for (let i = 0; i <= count; i += 1) {
+    content.demo.falls.video.particles.spawn({
+      life: 1,
+      offset: {x: engine.fn.randomFloat(-context.canvas.height/17/2, context.canvas.height/17/2), y: engine.fn.randomFloat(-context.canvas.height/17/2, 0)},
+      radius: engine.fn.randomFloat(0, 1),
+      rate: engine.fn.randomFloat(4, 8),
+      velocity: {x: velocity * engine.fn.randomFloat(0.5, 1) * -moveDirection, y: velocity * engine.fn.randomFloat(-0.5, 0)},
+      x: x - (moveDirection * 1),
+      y: 0,
+    })
+  }
 })
