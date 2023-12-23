bf465aacaa7bb2c07abcc276e9e942324ff28264
nicross <nicross@users.noreply.github.com>
2023-12-23 14:43:26 -0600

    Falls: Add move and shoot sounds

--- src/js/content/demo/falls/audio/playerMove.js
+++ src/js/content/demo/falls/audio/playerMove.js
@@ -1,5 +1,31 @@
 content.demo.falls.audio.playerMove = () => {
-  console.log('woosh')
+  const bus = content.demo.falls.audio.bus(),
+    context = engine.context(),
+    moveDirection = content.demo.falls.input.moveDirection()
+
+  const synth = engine.synth.amBuffer({
+    buffer: content.audio.buffer.brownNoise.choose(),
+    carrierGain: 0.5,
+    modDepth: 0.5,
+    modFrequency: engine.fn.randomFloat(8, 16),
+    playbackRate: engine.fn.randomFloat(0.5, 1),
+  }).filtered({
+    frequency: engine.fn.randomFloat(750, 1500),
+  }).chainAssign(
+    'panner', context.createStereoPanner()
+  ).connect(bus)
+
+  const detune = engine.fn.randomFloat(-10, 10),
+    duration = 1/8,
+    gain = engine.fn.fromDb(engine.fn.randomFloat(-12.5, -11.5)),
+    now = engine.time()
+
+  synth.panner.pan.linearRampToValueAtTime(-moveDirection, now + duration)
+
+  synth.param.gain.linearRampToValueAtTime(gain, now + 1/64)
+  synth.param.gain.linearRampToValueAtTime(engine.const.zeroGain, now + duration)
+
+  synth.stop(now + duration)
 }
 
 content.demo.falls.player.on('move', () => content.demo.falls.audio.playerMove())
--- src/js/content/demo/falls/audio/projectileShoot.js
+++ src/js/content/demo/falls/audio/projectileShoot.js
@@ -1,5 +1,29 @@
 content.demo.falls.audio.projectileShoot = () => {
-  console.log('pew')
+  const bus = content.demo.falls.audio.bus(),
+    rootFrequency = engine.fn.fromMidi(36)
+
+  const synth = engine.synth.fm({
+    carrierFrequency: rootFrequency,
+    carrierType: 'square',
+    modDepth: rootFrequency,
+    modFrequency: engine.fn.randomFloat(8, 16),
+    modType: 'sine',
+  }).filtered({
+    frequency: rootFrequency * 4,
+  }).connect(bus)
+
+  const detune = engine.fn.randomFloat(-10, 10),
+    duration = 1/8,
+    gain = engine.fn.fromDb(engine.fn.randomFloat(-12.5, -11.5)),
+    now = engine.time()
+
+  synth.param.detune.linearRampToValueAtTime(2400 + detune, now + 1/64)
+  synth.param.detune.linearRampToValueAtTime(detune, now + duration)
+
+  synth.param.gain.linearRampToValueAtTime(gain, now + 1/64)
+  synth.param.gain.linearRampToValueAtTime(engine.const.zeroGain, now + duration)
+
+  synth.stop(now + duration)
 }
 
 content.demo.falls.projectiles.on('shoot', () => content.demo.falls.audio.projectileShoot())
