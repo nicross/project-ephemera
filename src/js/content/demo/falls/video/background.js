2f544b852de61a55200a208debc1e556ee648833
nicross <nicross@users.noreply.github.com>
2024-01-13 19:42:27 -0600

    Falls: Improve color variations

--- src/js/content/demo/falls/video/background.js
+++ src/js/content/demo/falls/video/background.js
@@ -4,7 +4,7 @@ content.demo.falls.video.background = (() => {
       const context = content.demo.falls.video.context(),
         saturation = content.demo.falls.video.color.saturation()
 
-      context.fillStyle = `hsl(0deg, 0%, ${engine.fn.scale(saturation, 0.5, 1, 0.0625, 0) * 100}%)`
+      context.fillStyle = `hsl(0deg, 0%, ${engine.fn.scale(saturation, 0.25, 1, 0.1, 0) * 100}%)`
       context.fillRect(0, 0, context.canvas.width, context.canvas.height)
 
       return this
--- src/js/content/demo/falls/video/color.js
+++ src/js/content/demo/falls/video/color.js
@@ -1,8 +1,20 @@
 content.demo.falls.video.color = (() => {
   const hueField = engine.fn.createNoise({
-    octaves: 4,
+    octaves: 8,
     seed: ['falls', 'color', 'hue'],
     type: 'simplex2d',
+  })
+
+  const hueCenterField = engine.fn.createNoise({
+    octaves: 8,
+    seed: ['falls', 'color', 'hueCenter'],
+    type: '1d',
+  })
+
+  const hueRangeField = engine.fn.createNoise({
+    octaves: 8,
+    seed: ['falls', 'color', 'hueRange'],
+    type: '1d',
   })
 
   const saturationField = engine.fn.createNoise({
@@ -17,19 +29,21 @@ content.demo.falls.video.color = (() => {
         time = content.demo.falls.time.get()
 
       const value = engine.fn.lerp(
-        hueField.value(x / size * 4, time / 60),
-        hueField.value((x - size) / size * 4, time / 60),
+        hueField.value(x / (size/2), time / 30),
+        hueField.value((x - (size/2)) / (size/2), time / 30),
         x / size
       )
 
-      return engine.fn.wrap(
-        720 * value,
-        0, 360
-      )
+      const center = engine.fn.lerp(0, 720, hueCenterField.value(time / 60)),
+        range = engine.fn.lerp(0, 360, Math.abs((2 * hueRangeField.value(time / 60)) - 1))
+
+      return center + (Math.sin(value * engine.const.tau) * range/2)
     },
     load: function () {
       engine.ephemera
         .add(hueField)
+        .add(hueCenterField)
+        .add(hueRangeField)
         .add(saturationField)
 
       return this
@@ -38,17 +52,21 @@ content.demo.falls.video.color = (() => {
       const time = content.demo.falls.time.get()
 
       return engine.fn.lerp(
-        0.5, 1,
-        saturationField.value(time / 120)
+        0.25, 1,
+        saturationField.value(time / 45)
       )
     },
     unload: function () {
       hueField.reset()
+      hueCenterField.reset()
+      hueRangeField.reset()
       saturationField.reset()
 
       engine.ephemera
-        .add(hueField)
-        .add(saturationField)
+        .remove(hueField)
+        .remove(hueCenterField)
+        .remove(hueRangeField)
+        .remove(saturationField)
 
       return this
     },
