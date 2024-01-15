0ff62fce8d28467dd3497c1f31e052b7bda042b0
nicross <nicross@users.noreply.github.com>
2024-01-15 17:01:52 -0600

    Falls Implement pickup sounds

--- src/js/content/demo/falls/audio/pickupDestroy.js
+++ src/js/content/demo/falls/audio/pickupDestroy.js
@@ -1,5 +1,40 @@
 content.demo.falls.audio.pickupDestroy = (pickup) => {
-  console.log('destroy', pickup)
+  const relative = {
+    x: engine.fn.clamp(content.demo.falls.player.toRelativeX(pickup.x) / 8, -1, 1),
+    y: pickup.y,
+  }
+
+  const bus = content.demo.falls.audio.bus(),
+    context = engine.context(),
+    detune = engine.fn.randomFloat(-10, 10),
+    duration = 2,
+    rootFrequency = pickup.frequency
+
+  const synth = engine.synth.fm({
+    gain: engine.fn.fromDb(-9),
+    carrierDetune: detune,
+    carrierFrequency: rootFrequency,
+    carrierType: 'triangle',
+    modDepth: rootFrequency * Math.PI/2,
+    modDetune: detune,
+    modFrequency: rootFrequency * Math.PI/2,
+  }).filtered({
+    detune: detune,
+    frequency: rootFrequency,
+  }).chainAssign(
+    'panner', context.createStereoPanner()
+  ).connect(bus)
+
+  const now = engine.time()
+
+  synth.panner.pan.value = Math.cos(Math.atan2(relative.y, relative.x))
+
+  synth.param.detune.linearRampToValueAtTime(detune - 3600, now + duration/16)
+  synth.param.detune.linearRampToValueAtTime(detune - 4800, now + duration)
+  synth.param.gain.linearRampToValueAtTime(engine.fn.fromDb(-15), now + duration/16)
+  synth.param.gain.linearRampToValueAtTime(engine.const.zeroGain, now + duration)
+
+  synth.stop(now + duration)
 }
 
 content.demo.falls.pickups.on('destroy', ({pickup}) => content.demo.falls.audio.pickupDestroy(pickup))
